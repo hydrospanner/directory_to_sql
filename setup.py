ca758ab739fa7f0f2c3de0b5accbd11aa0ce9b43
hydrospanner <johntuckerusa@gmail.com>
2019-04-20 16:22:56 -0500

    Add README to setup long descrition.

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+
+with open('README.rst', 'r') as file:
+      long_description = file.read()
+
 setup(name='directory_to_sql',
-      version='0.1',
+      version='0.1.1',
       description='Get a SQL database of file directory data.',
+      long_description=long_description,
       url='http://github.com/hydrospanner/directory_to_sql',
       author='John Tucker',
       license='MIT',
